8b0969aa1a3bb765effddf49fea9ad6a92b35b23
Bankole Ona ara <richwaydev01@gmail.com>
2025-04-15 17:16:14 -0700

    desing

--- src/components/Auth/AuthNavBar.tsx
+++ src/components/Auth/AuthNavBar.tsx
@@ -4,7 +4,6 @@ import { useRouter } from "next/navigation";
 import Button from "@/Common/Button";
 import Image from "next/image";
 import Logo from "../../../public/Svg/Logo.svg";
-import { useState, } from "react";
 // import Hamburger from "../../../public/Svg/Hamburger.svg"
 
 const AuthNavBar = () => {
